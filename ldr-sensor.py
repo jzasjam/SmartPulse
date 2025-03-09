cb2e762fb8d0094a7f58cdeea2a569a58c3f4b95
jzasjam <jonathanzasadajames@gmail.com>
2025-03-09 01:24:49 +0000

    LDR code updated

--- ldr-sensor.py
+++ ldr-sensor.py
@@ -1,15 +1,13 @@
+# Source: Modified from https://gpiozero.readthedocs.io/en/latest/recipes.html#light-sensor 
+
 from gpiozero import LightSensor
+import time
 
 sensor = LightSensor(18)
 
-# Detect when it is light or dark
+# Continuously detect light level
 print("Press Ctrl+C To Stop")
 
 while True:
-	sensor.wait_for_light()
-	print("It's light! :)") # Print a notification
-	print(sensor.value)	# Print the value (between 0-1)
-	
-	sensor.wait_for_dark()
-	print("It's dark :(")
+	time.sleep(0.1) # Small delay between readings
 	print(sensor.value)
